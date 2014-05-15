38de009a9c64bcd1f70c95f3271aa70477f0fc2c
Frank Thompson <frankthompson@gmail.com>
2014-05-15 01:33:30 -0500

    added card class

--- hanabi.py
+++ hanabi.py
@@ -1,7 +1,40 @@
 import random
 
 CARDNUMS = '1'*15 + '2'*10 + '3'*10 + '4'*10 + '5'*5
-CARDCOLS = 'rbgyw'* 10
+CARDCOLS = 'RBGYW'*10
+
+class Card():
+	num = ''
+	color = ''
+
+	notColors = set()
+	notNums = set()
+
+	def __init__(self, info):
+		self.num = info[0]
+		self.color = info[1]
+
+	def __str__(self):
+		return str(self.num) + self.color
+
+	def setNotColor(self, color):
+		self.notColors.add(color)
+
+	def setNotNum(self, num):
+		self.notNums.add(num)
+
+	def setColor(self, color):
+		#should fix this later
+		for col in 'RBGYW':
+			if col != color:
+				self.notColors.add(col)
+
+	def setNum(self, num):
+		#should fix this later
+		for i in '12345':
+			if num != i:
+				self.notNums.add(i)
+
 
 class Hanabi():
 	deck = []
@@ -33,7 +66,7 @@ class Hanabi():
 	def randomizePlayers(self):
 		random.shuffle(self.players)
 
-	def startGame(self):
+	def playGame(self):
 		self.shuffle()
 		self.randomizePlayers()
 		if len(self.players) < 4:
@@ -50,14 +83,29 @@ class Hanabi():
 	def notify(self, player, msg):
 		print(player + ':', msg)
 
+	#asks player for input continually until valid move is given
+	#returns easily-parseable action of TBD formatting
 	def getInput(self, player):
 		pIn = input()
 		while not self.isValid(pIn):
 			self.notify(p, "invalid input!")
+		return pIn
+
+	def doAction(player, action):
+		if action[0] == "discard":
+			pass
+		elif action[0] == "play":
+			pass
+		elif action[0] == "tell":
+			pass
+		else:
+			print("OOPS!")
+			pass
+
 
 h = Hanabi()
 h.addPlayer('Frank')
 h.addPlayer('Alison')
 h.addPlayer('Arthur')
-self.startGame()
+self.playGame()
 h.viewHands()
