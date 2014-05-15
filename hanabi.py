dee5996cd4e218bd62e2a3f210ad35ec8d233973
Frank Thompson <frankthompson@gmail.com>
2014-05-14 23:48:14 -0500

    first commit

---
+++ hanabi.py
@@ -0,0 +1,54 @@
+import random
+
+CARDNUMS = '1'*15 + '2'*10 + '3'*10 + '4'*10 + '5'*5
+CARDCOLS = 'rbgyw'* 10
+
+class Hanabi():
+	deck = []
+	discard = []
+
+	piles = {}
+
+	players = []
+	hands = {}
+	
+	numTokens = 0
+	numBombs = 0
+
+	def __init__(self):
+		self.deck = list(zip(CARDNUMS, CARDCOLS))
+
+	def shuffle(self):
+		random.shuffle(self.deck)
+
+	def draw(self, numCards):
+		cardsDrawn = []
+		while len(self.deck) > 0 and len(cardsDrawn) < numCards:
+			cardsDrawn.append(self.deck.pop())
+		return cardsDrawn
+
+	def addPlayer(self, name):
+		self.players.append(name)
+
+	def randomizePlayers(self):
+		random.shuffle(self.players)
+
+	def startGame(self):
+		self.shuffle()
+		self.randomizePlayers()
+		if len(self.players) < 4:
+			handSize = 5
+		else:
+			handSize = 4
+		for p in self.players:
+			self.hands[p] = self.draw(handSize)
+
+	def viewHands(self):
+		for p in self.players:
+			print(p, self.hands[p])
+
+#h = Hanabi()
+#h.addPlayer('Frank')
+#h.addPlayer('Alison')
+#h.addPlayer('Arthur')
+#h.viewHands()
